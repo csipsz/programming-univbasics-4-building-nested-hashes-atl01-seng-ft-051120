2e92f1393824bc8f7fd8f00707079b3b6ed0526a
Luca Kanyo <lucakanyo@protonmail.com>
2020-03-29 17:54:42 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,8 +13,13 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-  
-
+   monopoly = {
+    :railroads => {
+      :pieces => 4, 
+      :rent_in_dollars => {},
+      :names => {}
+  }
+  }
 end
 
 def monopoly_with_fourth_tier
